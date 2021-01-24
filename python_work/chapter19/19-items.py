526011eb2266198c8dd86fa337af951dfece7bae
mal-admin <myautoloan-admin@myautoloan.com>
2021-01-23 19:28:54 -0700

    adding exercises-chapter 19

---
+++ python_work/chapter19/19-items.py
@@ -0,0 +1,20 @@
+# 19-3. Refactoring: There are two places in views.py where we make sure the user associated with a topic matches the currently logged in user. Put the code for this check in a function called check_topic_owner(), and call this function where appropriate.
+
+# - Complete
+
+#--------------------------
+
+# 19-4. Protecting new_entry: Currently, a user can add a new entry to another user’s learning log by entering a URL with the ID of a topic belonging to another user. Prevent this attack by checking that the current user owns the entry’s topic before saving the new entry.
+
+# - Complete
+
+#--------------------------
+
+
+# 19-1
+# 19-2
+# 19-5
+
+# I "cheated" on the above exercises... i have created other projects that require login and limiting access based on user
+
+
